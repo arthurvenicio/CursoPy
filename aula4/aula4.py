3746c5607a1d9bc236e153f19addea4308249093
Arthur Venicio <stuks.midia@gmail.com>
2021-07-21 11:28:54 -0300

    lesson 04

---
+++ aula4/aula4.py
@@ -0,0 +1,13 @@
+"""
+Types
+
+str = string = text
+int = integer = number (10, 20, -15 , -123, 15000)
+float = real = (10.23, 11.56, -45.65, -12.123)
+bool = boolean = (true or false) = (10 == 10) // true
+"""
+
+print('Arthur',  type('Arthur'))
+print(10,  type(10))
+print(25.43,  type(25.43))
+print('l' == 'l',  type('l' == 'l'))
---
+++ aula4/lesson.py
@@ -0,0 +1,11 @@
+# Name: string
+print('Arthur')
+
+# Age: number
+print(19, type(19))
+
+# Height: float
+print(1.71, type(1.71))
+
+# Of legal age
+print(19 > 18, type(19>18))
