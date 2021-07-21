d258ecbb16c68258720f5d4548fa51faeed43a75
Arthur Venicio <stuks.midia@gmail.com>
2021-07-21 09:21:45 -0300

    initial commit with lesson 01

---
+++ aula1/aula1.py
@@ -0,0 +1,14 @@
+# This is a comment
+print('Lesson 01')
+print('Lesson 01')
+# Another comment
+print('Lesson 01')  # One more comment
+print('Lesson 01')
+
+# or 
+"""
+print('Lesson 01')
+print('Lesson 01')
+print('Lesson 01')
+print('Lesson 01')
+"""
