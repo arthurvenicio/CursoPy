de0f3f46896a5ee258ff2e14700a7bd5334bb52e
Arthur Venicio <stuks.midia@gmail.com>
2021-07-21 10:58:16 -0300

    lesson 03

---
+++ aula3/aula3.py
@@ -0,0 +1,14 @@
+"""
+str = string
+"""
+
+# Recommend
+print('String iniciadas com aspas simples e com "aspas duplas" aqui')
+print("String iniciadas com aspas duplas e com 'aspas simples' aqui")
+
+# Escape character
+print("String iniciadas com aspas duplas e com \"aspas duplas\" aqui")
+print('String iniciadas com aspas simples e com \'aspas simples\' aqui')
+
+# You can but don't use, r = raw
+print(r"String iniciadas com aspas duplas e com \"aspas duplas\" aqui")
