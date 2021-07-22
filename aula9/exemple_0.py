283620b0f6d27d4bbd27fb1b6f5f905845c8a4b0
Arthur Venicio <arthur.venicio@alest.com.br>
2021-07-22 00:13:09 -0300

    lesson 09

---
+++ aula9/exemple_0.py
@@ -0,0 +1,9 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+
+born_year = 2021 - int(age)
+
+print()
+print(f'The user name is {name}')
+print(f'{name}\'s age is {age} old')
+print(f'{name}\'s born in {born_year}')
---
+++ aula9/exemple_1.py
@@ -0,0 +1,5 @@
+num1 = input('Insert one number: ')
+num2 = input('Insert one number: ')
+result = int(num1) + int(num2)
+
+print(f'The result is {result}')
