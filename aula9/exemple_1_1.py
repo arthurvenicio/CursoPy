f8fe629f76e9c67813ae6c1a7d90e0e64dc9a440
Arthur Venicio <stuks.midia@gmail.com>
2021-07-22 17:33:52 -0300

    lesson 09

---
+++ aula9/exemple_1_1.py
@@ -0,0 +1,13 @@
+if False:
+    print('True')
+    print('test test2')
+elif True:
+    print('Now is true')
+    name = input('What is your name? ')
+
+    print(f'Your name is {name}')
+elif False:
+    print('One more true.')
+    print(22 + 22)
+else:
+    print('Not is true')
