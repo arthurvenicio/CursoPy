a56ecea86dbe39bfdd4c44e0dc0b642153bf7484
Arthur Venicio <stuks.midia@gmail.com>
2021-07-28 11:48:47 -0300

    lesson 21

---
+++ aula21/aula21.py
@@ -0,0 +1,14 @@
+
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+
+else:  # When the condition becomes False
+    print('Finish here')
+# print('Finish here')
+
