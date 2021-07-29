c3e672cf9daa8c383edbab0853fa41f17f227667
Arthur Venicio <stuks.midia@gmail.com>
2021-07-29 10:21:40 -0300

    lesson 22

---
+++ aula22/aula22.py
@@ -0,0 +1,35 @@
+"""
+#Iteração 
+"""
+
+
+# frase = 'o rato roeu a roupa do rei de roma'
+# tamanho_frase = len(frase)
+# contador = 0
+# nova_string = ''
+#
+# while contador < tamanho_frase:
+#     # print(frase[contador])
+#     nova_string += frase[contador]
+#     print(nova_string)
+#     contador += 1
+#
+# print(nova_string)
+
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+input_do_usuario = input('Qual letra você deseja maiuscula? ')
+
+while contador < tamanho_frase:
+    letra = frase[contador]
+
+    if letra == input_do_usuario:
+        nova_string += input_do_usuario.upper()
+    else:
+        nova_string+= letra
+    contador += 1
+
+print(nova_string)
