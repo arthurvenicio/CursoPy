3792007687d0da4121e88cd29e65dbb6757b4975
Arthur Venicio <stuks.midia@gmail.com>
2021-08-02 11:18:29 -0300

    lesson 33 - functions #2

---
+++ aul26-50/aula33 - functions/aula33_2.py
@@ -0,0 +1,24 @@
+# def division(n1, n2):
+#     if n2 == 0:
+#         return
+#
+#     return n1 / n2
+#
+#
+# var = division(1, 3)
+# if var:
+#     print(var)
+# else:
+#     print('Conta invalida')
+
+def f(var):
+    print(var)
+
+def dumb():
+    return f
+
+# dumb()('Teste')
+
+vari = dumb()
+
+vari('Now here!')
