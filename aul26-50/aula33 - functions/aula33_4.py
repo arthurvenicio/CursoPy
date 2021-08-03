151ba67263fa4e1d9cd9e00b04599c20635eeafc
Arthur Venicio <stuks.midia@gmail.com>
2021-08-03 10:16:44 -0300

    lesson 33 - functions #4

---
+++ aul26-50/aula33 - functions/aula33_4.py
@@ -0,0 +1,13 @@
+def func(*args, **kwargs):
+    print(*args, sep='-')
+
+    age = kwargs.get('age')
+
+    if age is not None:
+        print(age)
+    else:
+        print('Don\'t have age here!')
+
+list = [1, 2, 3, 4, 5]
+
+func(*list, age=31)
