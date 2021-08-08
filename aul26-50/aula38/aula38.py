410c90d25275a96ee2a12ee6751180127584c9c0
arthurvenicio <stuks.midia@gmail.com>
2021-08-07 23:41:46 -0300

    lesson 38

---
+++ aul26-50/aula38/aula38.py
@@ -0,0 +1,27 @@
+# add, update, clear, discard
+# union or | (une)
+# intersection or & (todos os elementos nos dois sets)
+# difference or - (elementos apenas no set da esquerda)
+# symmetric_difference ^ (Elementos que estão nos dois sets, porém não em ambos)
+
+# s1 = set()  # Cria um set vazio
+# s1.add((1,2,3,4,5))
+# s1.add(('1,2,3,4,5'))
+# s1.discard(('1,2,3,4,5'))
+# print(s1)
+
+# s1 = {1,2,3,4,5,6}
+# s2 = {1,2,3,4,5,7}
+# s3 = s1 | s2
+#
+# s1 = {1,2,3,4,5,6}
+# s2 = {1,2,3,4,5,7}
+# s3 = s1 & s2
+#
+# s1 = {1,2,3,4,5,6}
+# s2 = {1,2,3,4,5,7}
+# s3 = s1 - s2
+#
+# s1 = {1,2,3,4,5,6}
+# s2 = {1,2,3,4,5,7}
+# s3 = s1 ^ s2
