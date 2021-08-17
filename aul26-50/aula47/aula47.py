589f6297cfafc3d610ea78dd8276a9cc31a1064a
arthurvenicio <stuks.midia@gmail.com>
2021-08-17 10:39:40 -0300

    lesson 47

---
+++ aul26-50/aula47/aula47.py
@@ -0,0 +1,20 @@
+"""
+Combinations, Permutations e Product - Itertools
+Combination - the order don't matter
+Permutation - the order matters
+@ They don't repeat unique values
+Product - The order matters and repeat unique values.
+"""
+
+from itertools import combinations, permutations, product
+
+pessoas = ['Arthur', 'José', 'Samuel', 'Laura', 'Carol', 'Samara', 'Gildo', 'Anderson']
+
+# for group in combinations(pessoas, 2):
+#     print(group)
+
+# for group in permutations(pessoas, 2):
+#     print(group)
+
+# for group in product(pessoas, repeat=2):
+#      print(group)
